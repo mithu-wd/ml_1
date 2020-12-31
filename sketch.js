c1962363b059b075f50b47eeee02ec895cd928a3
mithu-wd <56589470+mithu-wd@users.noreply.github.com>
2020-12-31 21:14:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -85,19 +85,38 @@ function setup() {
 function draw() {
     image(video,0,0);
     
-    for(let x=0;x<=video.width;x++){
-        for(let y=0;y<=video.height;y++){
-            video.loadPixels()
-        let clr=get(x,y)
-        if((clr[0]+clr[1]+clr[2])/3>=200){
-            point(x, y)
-            strokeWeight(0.5)
-            stroke("red")
-        }
+//     for(let x=0;x<=video.width;x++){
+//         for(let y=0;y<=video.height;y++){
+//             video.loadPixels()
+//         let clr=get(x,y)
+//         if((clr[0]+clr[1]+clr[2])/3>=200){
+//             point(x, y)
+//             strokeWeight(0.5)
+//             stroke("red")
+//         }
         
-        }
+//         }
 
+//     }
+//             video.updatePixels()
+    function draw() {
+  background(51);
+  video.loadPixels();
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (video.width - x + 1 + (y * video.width)) * 4;
+      var r = video.pixels[index + 0];
+      var g = video.pixels[index + 1];
+      var b = video.pixels[index + 2];
+      var bright = (r + g + b) / 3;
+      var w = map(bright, 0, 255, 0, 5);
+      noStroke();
+      fill(255);
+      rectMode(CENTER);
+      rect(x * 5, y * 5, w, w);
     }
-            video.updatePixels()
+  }
+
+}
     
 }
