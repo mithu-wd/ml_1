f83d4ab70b0884ea86439295c15e5ccb81dc267a
mithu-wd <56589470+mithu-wd@users.noreply.github.com>
2020-12-31 20:54:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,70 @@
+// var video;
+// let classifier;
+// // load the model
+// function preload() {
+//     classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/19dMgGoi/model.json');
+
+
+// }
+
+// function setup() {
+//     createCanvas(window.innerWidth, window.innerHeight);
+//     video = createCapture({
+//         audio: false,
+//         video: {
+//             facingMode: {
+//                 exact: "environment"
+//             }
+//         }
+//     });
+//     video.hide();
+
+//     classifyimage();
+
+// }
+
+
+// function classifyimage() {
+
+//     classifier.classify(video, gotResults);
+
+
+// }
+
+// function draw() {
+//     image(video,10,10);
+// if(rest=="scissor"){
+//     background(255,0,0);
+
+// }
+//  else if(rest=="stone"){
+//      background(0,255,0);
+
+
+
+// }
+//     else if(rest=="paper"){
+    
+//     background(0,0,255);
+//     }
+// }
+// var pro;
+// let rest;
+// function gotResults(error, results) {
+//     if (error) {
+//         console.error(error);
+//         return;
+//     }
+
+//     rest = (results[0].label);
+//     classifyimage();
+// }
+
 var video;
-let classifier;
-// load the model
-function preload() {
-    classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/19dMgGoi/model.json');
 
-
-}
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(200, 200);
     video = createCapture({
         audio: false,
         video: {
@@ -19,43 +75,13 @@ function setup() {
     });
     video.hide();
 
-    classifyimage();
 
 }
 
 
-function classifyimage() {
-
-    classifier.classify(video, gotResults);
-
-
-}
 
 function draw() {
     image(video,10,10);
-if(rest=="scissor"){
-    background(255,0,0);
-
-}
- else if(rest=="stone"){
-     background(0,255,0);
-
-
-
-}
-    else if(rest=="paper"){
     
-    background(0,0,255);
-    }
-}
-var pro;
-let rest;
-function gotResults(error, results) {
-    if (error) {
-        console.error(error);
-        return;
-    }
 
-    rest = (results[0].label);
-    classifyimage();
 }
