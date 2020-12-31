4bcf49f03311e7e0bb91e8df26367e856cc9eb2d
mithu-wd <56589470+mithu-wd@users.noreply.github.com>
2020-12-31 22:18:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,161 +1,53 @@
-// // var video;
-// // let classifier;
-// // // load the model
-// // function preload() {
-// //     classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/19dMgGoi/model.json');
+var videoScale = 16;
 
-
-// // }
-
-// // function setup() {
-// //     createCanvas(window.innerWidth, window.innerHeight);
-// //     video = createCapture({
-// //         audio: false,
-// //         video: {
-// //             facingMode: {
-// //                 exact: "environment"
-// //             }
-// //         }
-// //     });
-// //     video.hide();
-
-// //     classifyimage();
-
-// // }
-
-
-// // function classifyimage() {
-
-// //     classifier.classify(video, gotResults);
-
-
-// // }
-
-// // function draw() {
-// //     image(video,10,10);
-// // if(rest=="scissor"){
-// //     background(255,0,0);
-
-// // }
-// //  else if(rest=="stone"){
-// //      background(0,255,0);
-
-
-
-// // }
-// //     else if(rest=="paper"){
-    
-// //     background(0,0,255);
-// //     }
-// // }
-// // var pro;
-// // let rest;
-// // function gotResults(error, results) {
-// //     if (error) {
-// //         console.error(error);
-// //         return;
-// //     }
-
-// //     rest = (results[0].label);
-// //     classifyimage();
-// // }
-
-// var video;
-
-
-// function setup() {
-    
-//     createCanvas(500, 500);
-//     background("black")
-//     video = createCapture({
-//         audio: false,
-//         video: {
-//             facingMode: {
-//                 exact: "environment"
-//             }
-//         }
-//     });
-//     video.hide();
-
-
-// }
-
-
-
-// function draw() {
-//     image(video,0,0);
-    
-// //     for(let x=0;x<=video.width;x++){
-// //         for(let y=0;y<=video.height;y++){
-// //             video.loadPixels()
-// //         let clr=get(x,y)
-// //         if((clr[0]+clr[1]+clr[2])/3>=200){
-// //             point(x, y)
-// //             strokeWeight(0.5)
-// //             stroke("red")
-// //         }
-        
-// //         }
-
-// //     }
-// //             video.updatePixels()
-//     function draw() {
-//   background(51);
-//   video.loadPixels();
-//   for (var y = 0; y < video.height; y++) {
-//     for (var x = 0; x < video.width; x++) {
-//       var index = (video.width - x + 1 + (y * video.width)) * 4;
-//       var r = video.pixels[index + 0];
-//       var g = video.pixels[index + 1];
-//       var b = video.pixels[index + 2];
-//       var bright = (r + g + b) / 3;
-//       var w = map(bright, 0, 255, 0, 5);
-//       noStroke();
-//       fill(255);
-//       rectMode(CENTER);
-//       rect(x * 5, y * 5, w, w);
-//     }
-//   }
-
-// }
-    
-// }
-// Daniel Shiffman
-// https://youtu.be/rNqaw8LT2ZU
-// http://thecodingtrain.com
-
-var video;
-
-var vScale = 16;
+// Number of columns and rows in our system
+var cols, rows;
 
 function setup() {
   createCanvas(640, 480);
+
+  // Initialize columns and rows
+  cols = width/videoScale;
+  rows = height/videoScale;
+
   pixelDensity(1);
-video = createCapture({
-        audio: false,
-        video: {
-            facingMode: {
-                exact: "environment"
-            }
-        }
-    }); 
-    video.size(width / vScale, height / vScale);
+  video = createCapture(VIDEO);
+  video.size(cols, rows);
+  //video.hide();
+}
+
+function mousePressed() {
 }
 
 function draw() {
-  background(51);
+  background(0);
   video.loadPixels();
-  for (var y = 0; y < video.height; y++) {
-    for (var x = 0; x < video.width; x++) {
-      var index = (video.width - x + 1 + (y * video.width)) * 4;
-      var r = video.pixels[index + 0];
-      var g = video.pixels[index + 1];
-      var b = video.pixels[index + 2];
-      var bright = (r + g + b) / 3;
-        fill(r,g,b)
-        rect(x,y,10)
-   
+
+  // Begin loop for columns
+  for (var i = 0; i < cols; i++) {
+    // Begin loop for rows
+    for (var j = 0; j < rows; j++) {
+      // Reversing x to mirror the image
+      // In order to mirror the image, the column is reversed with the following formula:
+      // mirrored column = width - column - 1
+      var loc = ((cols - i - 1) + j * cols) * 4;
+      
+      // The functions red(), green(), and blue() pull out the three color components from a pixel.
+      var r = video.pixels[loc   ]; 
+      var g = video.pixels[loc + 1];
+      var b = video.pixels[loc + 2];
+
+      // A rectangle size is calculated as a function of the pixel's brightness. 
+      // A bright pixel is a large rectangle, and a dark pixel is a small one.
+      var sz = map((r+g+b)/3, 0, 255, 0, videoScale);
+      rectMode(CENTER);
+      fill(255);
+      noStroke();
+      // For every column and row, a rectangle is drawn at an (x,y) location scaled and sized by videoScale.
+      var x = i*videoScale;
+      var y = j*videoScale;
+      rect(x + videoScale/2, y + videoScale/2, sz, sz);
     }
   }
-
 }
+
